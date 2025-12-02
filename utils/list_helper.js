6c98a2af88ab2db200d1fc9014805e46c05bb03e
eddu fraguío <1526666+fraguio@users.noreply.github.com>
2025-12-02 14:28:41 +0100

    test(ex. 4.4): add totalLikes helper function and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, blog) => {
+    return accumulator + blog.likes;
+  };
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
